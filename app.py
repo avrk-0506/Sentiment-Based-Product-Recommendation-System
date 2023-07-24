cf8ec4c855400b42064d866842449eaeccd46213
avrk-0506 <121335012+avrk-0506@users.noreply.github.com>
2023-07-24 10:36:02 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 from model import SentimentRecommender
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 
 sent_reco_model = SentimentRecommender()
 
